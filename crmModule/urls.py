0bf507f752af91f1f2e97a8d6a259c801053fc58
Juan Plaza <jufeplasa@hotmail.com>
2023-09-17 19:41:54 -0500

    feat: add templates for authetification

--- crmModule/urls.py
+++ crmModule/urls.py
@@ -3,6 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login,name='login'),
+    path('philanth/', views.philanth, name='philanth'),
+    path('signin/', views.signin,name='signin'),
+    path('logout/', views.signout,name='signout'),
     path('menu/', views.menu,name='menu')
 ]
--- crmModule/views.py
+++ crmModule/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.http import HttpResponse
 # Create your views here.
 
@@ -16,8 +16,9 @@ def signup(request):
         if request.POST['password1']==request.POST['password2']:
             try:
                 user=User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                
                 user.save()
-                return render(request, 'login.html')
+                return redirect('signin')
             except:
                 return render(request, 'signup.html',
                               {
@@ -31,20 +32,26 @@ def signup(request):
                                   "error": 'Password do not match'
                               })
             
+def philanth(request):
+    return render (request, 'philanth.html')
 
-def login(request):
+def signin(request):
      if request.method == 'GET':
-        return render(request, 'login.html',
+        return render(request, 'signin.html',
                   {
                       'form': AuthenticationForm
                   })
      else:
         user= authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request,'login.html', {'form': AuthenticationForm, 'error': 'Username or password is incorrect' })
+            return render(request,'signin.html', {'form': AuthenticationForm, 'error': 'Username or password is incorrect' })
         else:
-            return menu()
-            
+            login(request, user)
+            return redirect('menu')
+
+def signout(request):
+    logout(request)
+    return redirect('home')      
 
 def home(request):
     return render(request, 'home.html')
