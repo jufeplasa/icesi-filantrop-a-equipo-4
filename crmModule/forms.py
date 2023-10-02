1e31d358394b2bd019036d6de8bf1c1bf625ea56
David Molta <dmh1802@gmail.com>
2023-10-02 11:42:06 -0500

    Advance

--- crmModule/forms.py
+++ crmModule/forms.py
@@ -5,3 +5,10 @@ class SponsorForm(ModelForm):
     class Meta:
         model= Sponsor
         fields = ['name', 'personType','contact_number','email','previousColab']
+        labels = {
+            'name': 'Nombre',
+            'personType': 'Tipo de persona',
+            'contact_number': 'Número de contacto',
+            'email': 'Correo electrónico',
+            'previousColab': '¿La persona ha colaborado antes con icesi?',
+        }
