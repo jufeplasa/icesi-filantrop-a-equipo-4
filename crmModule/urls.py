3bad4e7681907d447eb85e1c26cf6cc3787a2b2d
David Molta <dmh1802@gmail.com>
2023-10-09 00:13:24 -0500

    style: Sponsor styling

--- crmModule/urls.py
+++ crmModule/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from .views import views, sponsor, event
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin,name='signin'),
+    path('', views.signin,name='signin'),
     path('logout/', views.signout,name='signout'),
     path('menu/', views.menu,name='menu'),
     path('sponsor/', sponsor.sponsor, name='sponsor'),
     path('sponsor/register/', sponsor.register_sponsor,name='register_sponsor'),
     path('sponsor/<int:sponsor_id>/', sponsor.sponsor_detail,name='sponsor_detail'),
+    path('sponsor/home',views.home,name='homereg'),
     path('sponsor/<int:sponsor_id>/delete', sponsor.delete_sponsor,name='delete_sponsor'),
     path('event/', event.event, name='event'),
     path('event/register/', event.register_event,name='register_event'),
--- crmModule/views/views.py
+++ crmModule/views/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm 
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from ..models import Sponsor
 
 # Create your views here.
 
@@ -53,7 +54,11 @@ def signout(request):
     return redirect('home')      
 
 def home(request):
-    return render(request, 'home.html')
+    sponsors=Sponsor.objects.all()
+    return render (request, 'home.html',{
+        'sponsors':sponsors
+    })
+ 
 
 def menu(request):
     return render(request, 'menu.html')
