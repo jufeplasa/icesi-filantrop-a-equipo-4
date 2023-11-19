2aaa1b1e71de3e17c1e76431d039b2daa3ab82fb
Juan P Uribe <uribegomezjuanpablo@gmail.com>
2023-11-19 16:25:35 -0500

    feat: more methos and urls added to load info from excel's documents

--- crmModule/views/loadData.py
+++ crmModule/views/loadData.py
@@ -1,10 +1,8 @@
 from ..loadFiles import readExcel
-from django.shortcuts import render, redirect
-from ..models import Sponsor
-import json
+from django.shortcuts import render
+from ..models import Sponsor, Event, Official
 
 def loadSponsors (request):
-
     data = readExcel('../../excelDatos/SponsorData.xlsx')
 
     for obj_id, obj_data in data.items:
@@ -14,6 +12,32 @@ def loadSponsors (request):
             contact_number=obj_data['contact_number'],
             email=obj_data['email'],
             previous_colab=obj_data['previousColab']
+        )
+        obj.save
+
+    print("se logro")
+    return render(request,'home.html')
+
+def loadEvents (request):
+
+    data = readExcel('../../excelDatos/EventData.xlsx')
+
+    for obj_id, obj_data in data.items:
+        obj = Event(
+            ...
+        )
+        obj.save
+
+    print("se logro")
+    return render(request,'home.html')
+
+def loadOfficial (request):
+
+    data = readExcel('../../excelDatos/OfficialData.xlsx')
+
+    for obj_id, obj_data in data.items:
+        obj = Official(
+            ...
         )
         obj.save
 
