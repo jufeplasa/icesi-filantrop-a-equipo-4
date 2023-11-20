223ad2bca323a883a0f4fcd2e880e9754996a2b4
David Molta <dmh1802@gmail.com>
2023-11-20 10:02:20 -0500

    feat: Dinamic search, logout button, Route protection

--- crm/settings.py
+++ crm/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL= '/'
 
 import os
 
